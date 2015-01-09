a7b773ddd433d2031e7691067415f86c5a52c05e
Alexander Rukletsov <alex@mesosphere.io>
2015-01-09 11:27:08 +0100

    Renamed define guard to comply with our naming policy.

--- src/watcher/whitelist_watcher.hpp
+++ src/watcher/whitelist_watcher.hpp
@@ -16,8 +16,8 @@
  * limitations under the License.
  */
 
-#ifndef __WHITELIST_WATCHER_HPP__
-#define __WHITELIST_WATCHER_HPP__
+#ifndef __WATCHER_WHITELIST_WATCHER_HPP__
+#define __WATCHER_WHITELIST_WATCHER_HPP__
 
 #include <string>
 
@@ -57,4 +57,4 @@ private:
 } // namespace internal {
 } // namespace mesos {
 
-#endif // __WHITELIST_WATCHER_HPP__
+#endif // __WATCHER_WHITELIST_WATCHER_HPP__
