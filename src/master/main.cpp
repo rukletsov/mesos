34c49696f3fa826ccf6eb8801f9b0a2aa5b11e35
Alexander Rukletsov <rukletsov@gmail.com>
2015-04-24 14:04:45 +0200

    Wired up --allocator flag in master.
    Review: https://reviews.apache.org/r/33514

--- src/master/main.cpp
+++ src/master/main.cpp
@@ -202,7 +202,7 @@ int main(int argc, char** argv)
   }
 
   // Create an instance of allocator.
-  const std::string allocatorName = DEFAULT_ALLOCATOR;
+  const std::string allocatorName = flags.allocator;
   Try<Allocator*> allocatorInstance = Allocator::create(allocatorName);
 
   if (allocatorInstance.isError()) {
