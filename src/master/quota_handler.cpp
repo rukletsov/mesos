f8fb37ec5ad30b91545277eec656002d372ff1ef
Alexander Rukletsov <rukletsov@gmail.com>
2015-11-09 23:07:22 +0100

    Quota: Created a bare pipeline for quota set requests.
    Processing quota request consists of several stages: request validation, sanity check and so on. This patch creates a basic workflow for quota requests, while the stages are implemented in subsequent patches.
    
    Review: https://reviews.apache.org/r/38059

--- src/master/quota_handler.cpp
+++ src/master/quota_handler.cpp
@@ -16,15 +16,81 @@
 * limitations under the License
 */
 
+#include <mesos/resources.hpp>
+
+#include <mesos/quota/quota.hpp>
+
+#include <process/defer.hpp>
+#include <process/future.hpp>
+#include <process/http.hpp>
+
+#include <stout/protobuf.hpp>
+
 #include "master/master.hpp"
 
+namespace http = process::http;
+
+using std::string;
+
+using http::Accepted;
+using http::BadRequest;
+using http::Conflict;
+using http::OK;
+
+using process::Future;
+
+using mesos::quota::QuotaInfo;
 
 namespace mesos {
 namespace internal {
 namespace master {
 
-// TODO(joerg84): For right now this is a stub to enable cooperation.
-// It will be filled as part of MESOS-1791.
+Try<QuotaInfo> validateQuotaRequest(const http::Request& request)
+{
+  // TODO(alexr): Implement as part per MESOS-3199.
+
+  QuotaInfo quota;
+  return quota;
+}
+
+
+Future<http::Response> Master::QuotaHandler::set(
+    const http::Request& request) const
+{
+  // Authenticate and authorize the request.
+  // TODO(alexr): Check Master::Http::authenticate() for an example.
+
+  // Next, validate and convert the request to internal protobuf message.
+  Try<QuotaInfo> validate = validateQuotaRequest(request);
+  if (validate.isError()) {
+    return BadRequest(validate.error());
+  }
+
+  const QuotaInfo& quotaInfo = validate.get();
+
+  // Validate whether a quota request can be satisfied.
+  // TODO(alexr): Implement as per MESOS-3073.
+
+  // Populated master's quota-related local state. We do it before updating the
+  // registry in order to make sure that we are not already trying to satisfy a
+  // request for this role (since this is a multi-phase event).
+  // NOTE: We do not need to remove quota for the role if the registry update
+  // fails because in this case the master fails as well.
+  Quota quota{quotaInfo};
+  master->quotas[quotaInfo.role()] = quota;
+
+  // Update registry with the new quota.
+  // TODO(alexr): MESOS-3165.
+
+  // We are all set, grant the request.
+  // TODO(alexr): Implement as per MESOS-3073.
+  // TODO(alexr): This should be done after registry operation succeeds.
+
+  // Notfify allocator.
+  master->allocator->setQuota(quotaInfo.role(), quotaInfo);
+
+  return OK();
+}
 
 } // namespace master {
 } // namespace internal {
